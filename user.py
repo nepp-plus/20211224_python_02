785a85ab35602e3cad95507747373d7aec167284
nepp-plus <nepp.plus@gmail.com>
2021-12-24 15:42:11 +0900

    사용자 - 나이 계산 기능 추가

--- user.py
+++ user.py
@@ -3,3 +3,9 @@ class User:
         self.name = name
         self.birth_year = birth_year
         self.point = point
+        
+    def get_age(self, year):
+        age = year - self.birth_year + 1
+        return age
+    
+    
