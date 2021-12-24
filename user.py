747cf222549230c2e5062b49ed37f3d2972095f9
nepp-plus <nepp.plus@gmail.com>
2021-12-24 15:46:25 +0900

    사용자 메쏘드 2개 추가
    - 년도를 파라미터로 -> 나이를 리턴
    - 정보 출력 -> self의 메쏘드 활용

--- user.py
+++ user.py
@@ -8,4 +8,8 @@ class User:
         age = year - self.birth_year + 1
         return age
     
-    
+    def print_user_info(self):
+        print('===== 사용자 정보 출력 =====')
+        print(f'이름 : {self.name}')
+        print(f'나이 : {self.get_age(2021)}세')
+        print(f'보유포인트 : {self.point} P')
